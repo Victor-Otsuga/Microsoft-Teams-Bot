6cac2c15bc9f6cda8d8bd054ead91bf298181371
Victor Augusto Pereira dos Santos <105857027+Victor-Otsuga@users.noreply.github.com>
2023-10-20 18:20:45 -0300

    fix(disabled)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 from pymsteams import connectorcard
 
-while True:
+while False:
   # Crie a mensagem
   myTeamsMessage = connectorcard("https://etecspgov.webhook.office.com/webhookb2/31bbf84f-0bad-40b7-aba6-a138fad02317@ed38466c-b641-437d-9ae9-d801b829fa94/IncomingWebhook/b1b64bb50da34d829730c85cf8626c30/77ddb707-71fe-488c-af99-235e1329dd8f")
   myTeamsMessage.text("Mensagem teste")
