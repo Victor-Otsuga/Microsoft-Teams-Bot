da2b01b8bb5f056a32de8f414bb128e09b34ccb2
Victor Augusto Pereira dos Santos <105857027+Victor-Otsuga@users.noreply.github.com>
2023-10-20 16:23:53 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+import pymsteams
+myTeamsMessage = pymsteams.connectorcard("https://etecspgov.webhook.office.com/webhookb2/31bbf84f-0bad-40b7-aba6-a138fad02317@ed38466c-b641-437d-9ae9-d801b829fa94/IncomingWebhook/b1b64bb50da34d829730c85cf8626c30/77ddb707-71fe-488c-af99-235e1329dd8f")
+myTeamsMessage.text("Mensagem teste")
+myTeamsMessage.send()
